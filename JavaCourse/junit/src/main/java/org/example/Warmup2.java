888ad3116c0f852c964993b8644e1d749ba7cf5c
kozakmarcin <38139290+kozakmarcin@users.noreply.github.com>
2022-12-29 09:59:11 +0100

    Update Warmup2.java
    Linki

--- JavaCourse/junit/src/main/java/org/example/Warmup2.java
+++ JavaCourse/junit/src/main/java/org/example/Warmup2.java
@@ -1,28 +1,29 @@
 package org.example;
 
 public class Warmup2 {
-
+ 
+    // https://codingbat.com/prob/p167430
 
     public static boolean has271(int[] nums) {
         for(int i =0; i< nums.length-2;i++){
-            System.out.println(nums[i] + " " +nums[i+1] + " " + nums[i+2] );
             if (nums[i+1] == nums[i] + 5  && (nums[i+2] > nums[i] -4 && nums[i+2] < nums[i] +2 )) return true;
         }
         return false;
     }
 
 
-
-    public static boolean noTriples(int[] nums) {
+    // https://codingbat.com/prob/p170221
+    
+    public boolean noTriples(int[] nums) {
         for(int i =0; i< nums.length-2;i++){
             if (nums[i] == nums[i+1] && nums[i+1] == nums[i+2] ) return false;
         }
         return true;
     }
 
+    // https://codingbat.com/prob/p110019
 
-
-    public static int array667(int[] nums) {
+    public int array667(int[] nums) {
         int sum=0;
         for(int i =0; i< nums.length-1;i++){
             if (nums[i] == 6 &&( nums[i+1] == 6|| nums[i+1]==7)) sum++;
@@ -30,8 +31,15 @@ public class Warmup2 {
         return sum;
     }
 
-
-    public static String altPairs(String str) {
+    // https://codingbat.com/prob/p126212
+    
+    public String stringYak(String str) {
+        return str.replaceAll("yak", "");
+    }
+    
+    //https://codingbat.com/prob/p121596 
+    
+    public String altPairs(String str) {
      StringBuilder sb = new StringBuilder();
      char[] chars = str.toCharArray();
      for (int i = 0; i< chars.length; i++){
@@ -43,8 +51,9 @@ public class Warmup2 {
     }
 
 
-
-    public static String stringX(String str) {
+    // https://codingbat.com/prob/p171260
+    
+    public String stringX(String str) {
         if(str.length()<=1) return str;
         else {
             StringBuilder sb = new StringBuilder();
@@ -58,8 +67,9 @@ public class Warmup2 {
     }
 
 
-
-    public static int stringMatch(String a, String b) {
+    // https://codingbat.com/prob/p198640
+    
+    public int stringMatch(String a, String b) {
         int sum = 0;
         for(int i=0; i<Math.min( a.length()-1,b.length()-1);i++){
             if( a.substring(i,i+2).equals(b.substring(i,i+2))) sum++;
@@ -68,7 +78,8 @@ public class Warmup2 {
         return sum;
     }
 
-
+    // https://codingbat.com/prob/p136041
+    
     public boolean array123(int[] nums) {
         for(int i = 0; i<nums.length-2; i++){
             if (nums[i]==1 && nums[i+1]==2 && nums[i+2] ==3) return true;
@@ -77,18 +88,20 @@ public class Warmup2 {
 
     }
 
-    public static boolean arrayFront9(int[] nums) {
+    // https://codingbat.com/prob/p186031
+    
+    public boolean arrayFront9(int[] nums) {
         int lenght=0;
         lenght=  nums.length < 4 ? nums.length:  4;
-
-
         for (int i = 0; i<lenght; i++){
             if (nums[i]==9) return true;
         }
         return false;
     }
 
-    public static int arrayCount9(int[] nums) {
+    // https://codingbat.com/prob/p184031
+    
+    public int arrayCount9(int[] nums) {
         int sum = 0;
         for(int n :nums){
            if (n==9) sum++ ;
@@ -97,8 +110,9 @@ public class Warmup2 {
     }
 
 
-
-    public static int last2(String str) {
+    // https://codingbat.com/prob/p178318
+    
+    public int last2(String str) {
         int sum = 0;
         for(int i = 0; i< str.length()-2; i++){
             if (str.regionMatches(i,str.substring(str.length()-2),0,2)) sum+=1;
@@ -108,8 +122,9 @@ public class Warmup2 {
 
 
 
-
-    public static String stringSplosion(String str) {
+    // https://codingbat.com/prob/p117334
+    
+    public String stringSplosion(String str) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i<=str.length(); i++){
             sb.append(str.substring(0,i));
@@ -118,8 +133,9 @@ public class Warmup2 {
     }
 
 
-
-     public static String stringBits(String str) {
+    // https://codingbat.com/prob/p165666
+    
+     public String stringBits(String str) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i<str.length(); i+=2){
             sb.append(str.charAt(i));
@@ -127,29 +143,37 @@ public class Warmup2 {
         return sb.toString();
 
     }
+    
+    // https://codingbat.com/prob/p186759
 
-
-   static boolean doubleX(String str) {
+   public boolean doubleX(String str) {
        if (str.contains("xx")&& (str.indexOf("x")==str.indexOf("xx"))) return true;
        return false;
     }
 
 
-
-    public static int countXX(String str) {
+    
+    //https://codingbat.com/prob/p194667   
+    
+    public int countXX(String str) {
         int sum = 0;
         for(int i = 0; i< str.length(); i++){
             if (str.regionMatches(i,"xx",0,)) sum+=1;
         }
         return sum;
     }
-
+    
+    // https://codingbat.com/prob/p101475
+    
     public String frontTimes(String str, int n) {
         if (n==0) return "";
         else if (str.length()<3 ) return frontTimes(str, n-1) +str;
         else return frontTimes(str.substring(0,3), n-1) +str.substring(0,3);
     }
 
+    
+     // https://codingbat.com/prob/p142270
+    
     public String stringTimes(String str, int n) {
         if (n==0) return "";
         return stringTimes(str, n-1) +str;
