8dcd0cd9e675cd4b75d85aed57d94f1f9b0d264b
kozakmarcin <38139290+kozakmarcin@users.noreply.github.com>
2022-12-29 11:36:38 +0100

    Update String1.java
    Poprawki plus linki do zadań

--- JavaCourse/junit/src/main/java/org/example/String1.java
+++ JavaCourse/junit/src/main/java/org/example/String1.java
@@ -2,38 +2,103 @@ package org.example;
 
 public class String1 {
 
-    public static String withoutX2(String str) {
+    // https://codingbat.com/prob/p151359
+    
+    public String withoutX2(String str) {
         if (str.length()>=2  && str.startsWith("x")&& str.charAt(1)=='x' ) return str.substring(2);
         else if ( str.length()>=1 && str.startsWith("x") ) return str.substring(1);
         else if (str.length()>2  && str.charAt(1)=='x' ) return str.charAt(0)+ str.substring(2);
         return str;
     }
 
-
-    public static String withoutX(String str) {
+    // https://codingbat.com/prob/p151940
+    
+    public String withoutX(String str) {
         if (str.startsWith("x")&& str.endsWith("x") && str.length()>1) return str.substring(1,str.length()-1);
         else if (str.startsWith("x")) return str.substring(1);
         else if (str.endsWith("x")) return str.substring(0,str.length()-1);
         return str;
-
     }
+    
+    
 
+    // https://codingbat.com/prob/p141494
 
-
-    public static String startWord(String str, String word) {
-
+    public String startWord(String str, String word) {
         if (word.length()>1 && str.regionMatches(1,word.substring(1),0,word.length()-1)) return str.substring(0,word.length());
         else if (word.length() ==1 && str.length()>0)return str.substring(0,1);
         return "";
     }
-
-    public static String conCat(String a, String b) {
-        if (a.charAt(a.length()-1)==b.charAt(0)) return a.concat(b.substring(1));
-        return a.concat(b);
-
+    
+    https://codingbat.com/prob/p110141
+    
+    public String deFront(String str) {    
+        if  (str.startsWith("ab")) return str;  
+        else if (str.startsWith("a")) return "a"+str.substring(2);
+        else if (str.charAt(1)=='b')  return "b"+str.substring(2);
+        return str.substring(2);
+    }
+    
+    // https://codingbat.com/prob/p142247
+    
+    public String without2(String str) {
+        if (str.length()>2 && str.endsWith(str.substring(0,2))) return str.substring(2);
+        else if (str.length()>2 || str.length()==1) return str;
+        return "";
+    }
+    
+    // https://codingbat.com/prob/p172063
+    
+    public String extraFront(String str) {
+        if(str.length()>2) return str.substring(0,2)+str.substring(0,2)+str.substring(0,2);
+        return str + str + str;
     }
 
+    
+    // https://codingbat.com/prob/p105745
+    
+    public String minCat(String a, String b) {
+        if(a.length()>b.length()) return a.substring(a.length()-b.length())+b;
+        return a+b.substring(b.length()-a.length());
+    }
 
+    
+    // https://codingbat.com/prob/p196652
+    
+    public boolean frontAgain(String str) {
+        if (str.length()>1) return  str.endsWith(str.substring(0,2));
+        return false;
+    }
+    
+    // https://codingbat.com/prob/p199216
+    
+    public String seeColor(String str) {
+        if (str.startsWith("red")) return "red";
+        else if (str.startsWith("blue")) return "blue";
+        return "";
+    }
+    
+    // https://codingbat.com/prob/p194786
+    
+    public String lastTwo(String str) {
+        if (str.length() >=2) {
+            char c1 = str.charAt(str.length()-1);
+            char c2 = str.charAt(str.length()-2);
+            return str.substring(0, str.length()-2) + c1 + c2;
+        } else if (str.length() ==1) return str;
+        return "";
+    }
+
+    
+    // https://codingbat.com/prob/p132118
+    
+   public String conCat(String a, String b) {
+        if (a.length()>0&& b.length()>0 &&a.charAt(a.length()-1)==b.charAt(0)) return a.concat(b.substring(1));
+        return a.concat(b);
+    }
+
+    // https://codingbat.com/prob/p138183
+    
     public String lastChars(String a, String b) {
         String s = "";
         if (a.isEmpty()) s="@";
@@ -43,7 +108,8 @@ public class String1 {
         return s;
     }
 
-
+    // https://codingbat.com/prob/p139076
+    
     public String atFirst(String str) {
         int len = str.length();
         if (len <2) {
@@ -55,13 +121,15 @@ public class String1 {
         return str.substring(0,2);
     }
 
-    public static boolean hasBad(String str) {
+    // https://codingbat.com/prob/p139075
+    
+    public boolean hasBad(String str) {
         return (str.startsWith("bad")|| str.startsWith("bad",1));
     }
 
+    // https://codingbat.com/prob/p115863
 
-
-    public static String middleThree(String str) {
+    public String middleThree(String str) {
         for(int i=0; i<str.length()/2;i++){
             String s1 = str.substring(0,i);
             String s2 = str.substring(str.length()-i);
@@ -70,65 +138,119 @@ public class String1 {
         return "";
 
     }
-//    public static String twoChar(String str, int index) {
-        if (str.length()-2 < index) return str.substring(0,2);
+    
+    // https://codingbat.com/prob/p144623
+    
+    public String twoChar(String str, int index) {
+        if (str.length()-2 < index || index<0) return str.substring(0,2);
         return str.substring(index,index+2);
     }
+    
+    // https://codingbat.com/prob/p174148
 
+    public String nTwice(String str, int n) {
+        return str.substring(0,n)+str.substring(str.length()-n);
+    }
 
-    public static String middleTwo(String str) {
+    // https://codingbat.com/prob/p103895
+
+    public boolean endsLy(String str) {
+        return str.endsWith("ly");
+    }
+
+    // https://codingbat.com/prob/p137729
+
+    public String middleTwo(String str) {
         return str.substring(str.length()/2-1,str.length()/2+1);
     }
 
+    // https://codingbat.com/prob/p174254
 
-    public static String withouEnd2(String str) {
+    public String withouEnd2(String str) {
         return (str.length()<2)? "" :str.substring(1,str.length()-1);
     }
 
-    public static String theEnd(String str, boolean front) {
+    // https://codingbat.com/prob/p162477
+
+    public String theEnd(String str, boolean front) {
         return (front)? str.substring(0,1) : str.substring(str.length()-1);
     }
 
-    public static String left2(String str) {
+    // https://codingbat.com/prob/p130781
+
+    public String right2(String str) {
+        return str.substring(str.length()-2)+str.substring(0,str.length()-2);
+    }
+
+    // https://codingbat.com/prob/p197720
+
+    public String left2(String str) {
         return str.substring(str.length()-2)+str.substring(0,str.length()-2);
     }
 
 
-    public static String nonStart(String a, String b) {
+    // https://codingbat.com/prob/p143825
+
+    public String nonStart(String a, String b) {
         return a.substring(1) + b.substring(1);
     }
+    
+    //     https://codingbat.com/prob/p168564
 
-    public static String comboString(String a, String b) {
+    public String comboString(String a, String b) {
         if (a.length()<b.length()) return a+b+a;
         return b+a+b;
     }
 
 
+    // https://codingbat.com/prob/p130896
+    
+    public String withoutEnd(String str) {
+        return str.substring(1,str.length()-1);
+    }
+    
+    // https://codingbat.com/prob/p172267
 
-    public static String spinWords(String sentence) {
-        StringBuilder sb1 = new StringBuilder();
-
-        for (String s: sentence.split(" ")) {
-            if (s.length()<5) sb1.append(s +" ");
-            else {
-                sb1.append(new StringBuilder(s).reverse().toString()+" ");
-            }
-        }
-        sb1.delete(sb1.length()-1,sb1.length());
-        return sb1.toString();
-
+    public String firstHalf(String str) {
+        return str.substring(0,str.length()/2);
     }
 
-    public static String firstTwo(String str) {
+    // https://codingbat.com/prob/p163411
+
+    public String firstTwo(String str) {
         if(str.length() < 2) return str;
         return str.substring(0,2);
     }
-    public static String extraEnd(String str) {
+
+    // https://codingbat.com/prob/p108853
+
+    public String extraEnd(String str) {
         return str.substring(str.length()-2)+str.substring(str.length()-2)+str.substring(str.length()-2);
     }
 
+    // https://codingbat.com/prob/p184030    
 
-    public static String makeOutWord(String out, String word) {
+    public String makeOutWord(String out, String word) {
         return out.substring(0,2) + word + out.substring(2,4);
+    }
+
+    // https://codingbat.com/prob/p147483
+
+    public String makeTags(String tag, String word) {
+        String tagbegin = "<"+tag+">";
+        String tagender = "</"+tag+">";
+        return tagbegin + word + tagender;
+    }
+
+    // https://codingbat.com/prob/p161056
+
+    public String makeAbba(String a, String b) {
+        return a + b + b +a;
+    }
+
+    // https://codingbat.com/prob/p171896
+
+    public String helloName(String name) {
+        return "Hello "+ name+"!";
     }
 }
