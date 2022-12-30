0f390aa844c0ec86f7484f3da6d5fd1f7cdc58d4
kozakmarcin <38139290+kozakmarcin@users.noreply.github.com>
2022-12-30 12:09:54 +0100

    Update Array2.java
    Dodane linki

--- JavaCourse/junit/src/main/java/org/example/Array2.java
+++ JavaCourse/junit/src/main/java/org/example/Array2.java
@@ -3,7 +3,9 @@ package org.example;
 import java.util.Arrays;
 
 public class Array2 {
-
+    
+    // https://codingbat.com/prob/p153059
+    
     public String[] fizzBuzz(int start, int end) {
         String[] fizzBuzzChalange = new String[end-start];
         int number = start;
@@ -14,12 +16,14 @@ public class Array2 {
             else fizzBuzzChalange[i] = ""+number;
             number++;
         }
-        System.out.println(Arrays.toString(fizzBuzzChalange));
+        
         return fizzBuzzChalange;
     }
+    
+    // https://codingbat.com/prob/p105771
 
     public int[] evenOdd(int[] nums) {
-        int[] newNums = new int[nums.length];
+       int[] newNums = new int[nums.length];
         int indexEven=0;
         int indexOdd = nums.length-1;
         for (int i = 0; i < nums.length; i++) {
@@ -30,22 +34,22 @@ public class Array2 {
                 newNums[indexOdd]=nums[i];
                 indexOdd--;
             }
-
-
         }
-        System.out.println(Arrays.toString(newNums));
         return newNums;
-
     }
+    
+    // https://codingbat.com/prob/p187050
+    
     public int[] zeroMax(int[] nums) {
         int maxOdd = 0;
         for (int i = nums.length-1; i >=0 ; i--) {
             if(nums[i]%2 != 0 && nums[i]>maxOdd) maxOdd=nums[i];
             if(nums[i]==0) nums[i] = maxOdd;
         }
-//        System.out.println(Arrays.toString(nums));
         return nums;
     }
+    
+    https://codingbat.com/prob/p196976
 
     public int[] withoutTen(int[] nums) {
         int[] newNums = new int[nums.length];
@@ -55,23 +59,11 @@ public class Array2 {
                 indexTen++;
                 newNums[nums.length-indexTen]=0;
             } else newNums[i-indexTen]=nums[i];
-
-
         }
-//        System.out.println(Arrays.toString(newNums));
         return newNums;
-//
-//        for (int i = nums.length-1; i >= 0; i--) {
-//            if( nums[i]==10) {
-//                tentemp = i;
-//                while (i<nums.length-1 && nums[i]!=0 ) i++;
-//                nums[tentemp] = nums[i];
-//                nums[i]=0;
-//                i= tentemp;
-//            }
-//        }
-
     }
+    
+    // https://codingbat.com/prob/p193753
 
     public int[] zeroFront(int[] nums) {
         int indexZero = -1;
@@ -85,6 +77,7 @@ public class Array2 {
         return nums;
     }
 
+    https://codingbat.com/prob/p169506
 
     public int[] notAlone(int[] nums, int val) {
         for (int i = 1; i <nums.length-1 ; i++) {
@@ -92,6 +85,8 @@ public class Array2 {
         }
         return nums;
     }
+    
+    // https://codingbat.com/prob/p164144
 
     public int[] post4(int[] nums) {
         int[] array = new int[0];
@@ -103,7 +98,7 @@ public class Array2 {
         return array;
     }
 
-
+    // https://codingbat.com/prob/p100246
 
     public int[] pre4(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
@@ -112,6 +107,8 @@ public class Array2 {
         }
         return new int[0];
     }
+    
+    // https://codingbat.com/prob/p199484
 
     public int[] tenRun(int[] nums) {
         int ten = -1;
@@ -121,6 +118,8 @@ public class Array2 {
         }
         return nums;
     }
+    
+    // https://codingbat.com/prob/p105031
 
     public int[] shiftLeft(int[] nums) {
         if (nums.length==0) return nums;
@@ -132,7 +131,7 @@ public class Array2 {
         return nums;
     }
 
-
+    // https://codingbat.com/prob/p142539
 
     public int[] fizzArray3(int start, int end) {
         int[] fizz = new int[end-start];
@@ -142,39 +141,42 @@ public class Array2 {
         return fizz;
     }
 
+    // https://codingbat.com/prob/p137874
+    
     public boolean tripleUp(int[] nums) {
         for (int i = 0; i < nums.length-2; i++) {
             if(nums[i+1]==nums[i]+1&& nums[i+2]==nums[i+1]+1) return true;
         }
         return false;
     }
+    
+    // https://codingbat.com/prob/p134300
+    
     public boolean sameEnds(int[] nums, int len) {
-        int[] array1 = Arrays.copyOfRange(nums,0,len);
+        int[] array1 = Arrays.copyOfRange(nums,0, len);
         int[] array2 = Arrays.copyOfRange(nums,nums.length-len, nums.length);
-        System.out.println(Arrays.toString(array1));
-        System.out.println(Arrays.toString(array2));
         return Arrays.equals(array1,array2);
     }
 
+    // https://codingbat.com/prob/p102145
 
     public boolean twoTwo(int[] nums) {
         boolean flag = false;
         if (nums.length==0) return true;
         else if (nums.length==1 && nums[0] !=2) return true;
         for (int i = 0; i < nums.length-1; i++) {
-
             if (nums[i] == 2 && nums[i+1]==2) {
                 flag=true;
                 i++;
             } else if( nums[i] == 2 && nums[i + 1] != 2) return false;
             else if( nums[i] != 2 && nums[i + 1] == 2) flag = false;
             else if( nums[i] != 2 && nums[i + 1] != 2) flag = true;
-
         }
         return flag;
-//
     }
 
+    // https://codingbat.com/prob/p109783
+    
     public boolean haveThree(int[] nums) {
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -184,6 +186,7 @@ public class Array2 {
         return sum==3;
     }
 
+    // https://codingbat.com/prob/p159979
 
     public boolean modThree(int[] nums) {
         for (int i = 0; i < nums.length-2; i++) {
@@ -192,6 +195,9 @@ public class Array2 {
         }
         return false;
     }
+    
+    // https://codingbat.com/prob/p169260
+    
     public boolean has12(int[] nums) {
         boolean flag1 = false;
         for (int i = 0; i < nums.length; i++) {
@@ -200,17 +206,17 @@ public class Array2 {
         }
         return false;
     }
+    
+    // https://codingbat.com/prob/p168357
+    
     public boolean has77(int[] nums) {
         boolean flag1 = false;
         boolean flag2 = false;
-
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 7 && (flag1 || flag2)) return true;
             else if (nums[i]!=7 && flag1) {
-//                System.out.println("i:"+i + "flag 2: " + flag2);
                 flag2 = true;
                 flag1= false;
-//                System.out.println("i:"+ i + "flag 2: " + flag2);
             }
             else {
                 flag1 = nums[i] == 7;
@@ -220,6 +226,7 @@ public class Array2 {
         return false;
     }
 
+    // https://codingbat.com/prob/p136254
 
     public int matchUp(int[] nums1, int[] nums2) {
         int sum=0;
@@ -229,7 +236,7 @@ public class Array2 {
         return sum;
     }
 
-
+    // https://codingbat.com/prob/p191878
 
     public boolean either24(int[] nums) {
         boolean flag2 = false;
@@ -241,21 +248,17 @@ public class Array2 {
         if(flag2 && flag4) return false;
         else return flag2 || flag4;
     }
-
+    
+    // https://codingbat.com/prob/p110222
 
     public boolean isEverywhere(int[] nums, int val) {
-
         for (int i = 0; i < nums.length-1; i++) {
             if( nums[i] != val || nums [i+1]!=val) return false;
-
-
         }
         return true;
-
     }
 
-
-
+    // https://codingbat.com/prob/p136648
 
     public boolean no14(int[] nums) {
         boolean flag1= false;
@@ -266,6 +269,9 @@ public class Array2 {
         }
         return !(flag1&&flag4);
     }
+    
+    // https://codingbat.com/prob/p178353
+    
     public String[] fizzArray2(int n) {
         String[] fizz = new String[n];
         for (int i = 0; i < n; i++) {
@@ -273,7 +279,9 @@ public class Array2 {
         }
         return fizz;
     }
-
+    
+    // https://codingbat.com/prob/p186672
+    
     public boolean only14(int[] nums) {
         for (int i : nums) {
             if (i!=1 && i !=4) return false;
@@ -281,7 +289,8 @@ public class Array2 {
         return true;
     }
 
-
+    // https://codingbat.com/prob/p178353
+    
     public int[] fizzArray(int n) {
         int[] fizz = new int[n];
         for (int i = 0; i < n; i++) {
@@ -290,7 +299,7 @@ public class Array2 {
         return fizz;
     }
 
-
+    // https://codingbat.com/prob/p104627
 
     public boolean more14(int[] nums) {
         int sum1 = 0;
@@ -301,6 +310,8 @@ public class Array2 {
         }
         return sum1>sum4;
     }
+    
+    // https://codingbat.com/prob/p194525
 
     public boolean lucky13(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
@@ -309,11 +320,10 @@ public class Array2 {
         return true;
     }
 
-
+    // https://codingbat.com/prob/p121853
 
     public boolean has22(int[] nums) {
         boolean flag1 = false;
-
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 2 && flag1) return true;
             else if (nums[i]==2) flag1 = true;
@@ -322,20 +332,20 @@ public class Array2 {
         return false;
     }
 
+    // https://codingbat.com/prob/p111327
 
     public int sum67(int[] nums) {
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 6) sum+=nums[i];
-
             else while ( nums[i]!=7){
                 i++;
             }
-
         }
         return sum;
     }
-
+    
+    // https://codingbat.com/prob/p127384
 
     public int sum13(int[] nums) {
         if (nums.length == 0) return 0;
@@ -343,6 +353,28 @@ public class Array2 {
         for (int i = 0; i < nums.length; i++) {
             if(i>0 && (nums[i] !=13 || nums[i-1] !=13)) sum+=nums[i];
             if(i==0 && nums[i]!=13) sum+=nums[i];
+        }
+        return sum;
+    }
+    
+    // https://codingbat.com/prob/p136585
+    
+    public int centeredAverage(int[] nums) {
+        return (Arrays.stream(nums).sum() - Arrays.stream(nums).max().getAsInt()-Arrays.stream(nums).min().getAsInt())/(nums.length-2);
+    }
+    
+    // https://codingbat.com/prob/p196640
+    
+    public int bigDiff(int[] nums) {
+        return (Arrays.stream(nums).max().getAsInt()- Arrays.stream(nums).min().getAsInt());
+    }
+    
+    // https://codingbat.com/prob/p162010
+    
+    public int countEvens(int[] nums) {
+        int sum = 0;
+        for(int i: nums){
+            if(i%2 ==0) sum++;
         }
         return sum;
     }
