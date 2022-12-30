c170d032c42a6ac15af071c501872f04d1a5b52e
kozakmarcin <38139290+kozakmarcin@users.noreply.github.com>
2022-12-30 08:59:22 +0100

    Update Recursion.java
    Dodane linki

--- JavaCourse/junit/src/main/java/org/example/Recursion.java
+++ JavaCourse/junit/src/main/java/org/example/Recursion.java
@@ -1,11 +1,16 @@
 package org.example;
 
 public class Recursion {
+    
+    // https://codingbat.com/prob/p118230
+    
     public String noX(String str) {
         if(str.length()<1 ) return "";
         if (str.charAt(str.length()-1)=='x') return noX(str.substring(0, str.length() - 1)) + "";
         return noX(str.substring(0, str.length()-1))+ str.charAt(str.length()-1);
     }
+    
+    // https://codingbat.com/prob/p170924
 
     public String changePi(String str) {
         if(str.length()<1 ) return "";
@@ -15,12 +20,15 @@ public class Recursion {
         else return  changePi(str.substring(0, str.length() - 1) )+ str.charAt(str.length()-1);
     }
 
+    // https://codingbat.com/prob/p101372
 
     public String changeXY(String str) {
         if(str.length()<1 ) return "";
         if (str.charAt(str.length()-1)=='x') return changeXY(str.substring(0, str.length() - 1)) + "y" ;
         else return  changeXY(str.substring(0, str.length() - 1)) + str.charAt(str.length()-1);
     }
+    
+    // https://codingbat.com/prob/p184029
 
     public int countHi(String str) {
         if(str.length()<3 && !str.equals("hi")) return 0;
@@ -28,6 +36,8 @@ public class Recursion {
         if (substring.equals("hi")) return 1 +countHi(str.substring(0, str.length() - 1));
         return countHi(str.substring(0, str.length() - 1));
     }
+    
+    // https://codingbat.com/prob/p170371
 
     public int countX(String str) {
         if(str.length()==0) return 0;
@@ -35,12 +45,15 @@ public class Recursion {
         if (str.charAt(str.length()-1)=='x') return 1+countX(substring);
         return countX(substring);
     }
-
+    
+    // https://codingbat.com/prob/p158888
 
     public int powerN(int base, int n) {
         if(n == 0) return 1;
         return base*powerN(base,n-1);
     }
+    
+    // https://codingbat.com/prob/p192383
 
     public int count8(int n) {
         if (n==0) return 0;
@@ -50,15 +63,16 @@ public class Recursion {
         }
         return count8(n/=10);
     }
-
+    
+    // https://codingbat.com/prob/p101409
 
     public int count7(int n) {
-
-
         if (n==0) return 0;
         if (n%10==7)return  1+count7(n/=10);
         return count7(n/=10);
     }
+    
+    // https://codingbat.com/prob/p163932
 
     public int sumDigits(int n) {
         if (n==0) return 0;
@@ -66,31 +80,38 @@ public class Recursion {
         return last+sumDigits(n/=10);
     }
 
+    // https://codingbat.com/prob/p194781
+    
     public int triangle(int rows) {
         if (rows==0) return 0;
-
         return rows+triangle(rows-1);
     }
+    
+    // https://codingbat.com/prob/p107330
+    
     public int bunnyEars2(int bunnies) {
         if (bunnies==0) return 0;
         if (bunnies%2 !=0) return 2+bunnyEars2(bunnies-1);
         else return 3+bunnyEars2(bunnies-1);
     }
-
+    
+    // https://codingbat.com/prob/p120015
+    
     public int fibonacci(int n) {
         if (n == 0) return 0;
         else if (n== 1) return 1;
         return fibonacci(n-1) + fibonacci(n-2);
     }
 
+    // https://codingbat.com/prob/p183649
 
     public int bunnyEars(int bunnies) {
         if (bunnies == 0) return 0;
-
         return 2+bunnyEars(bunnies-1);
     }
 
-
+    // https://codingbat.com/prob/p154669
+    
     public int factorial(int n) {
         if (n>0){
             return n*factorial(n-1);
