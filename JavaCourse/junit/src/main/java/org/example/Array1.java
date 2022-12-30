27c5f8acbef9475436bca02518f64448b2b5a1ae
kozakmarcin <38139290+kozakmarcin@users.noreply.github.com>
2022-12-30 08:36:50 +0100

    Update Array1.java
    Dodanie linków

--- JavaCourse/junit/src/main/java/org/example/Array1.java
+++ JavaCourse/junit/src/main/java/org/example/Array1.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Array1 {
 
+    // https://codingbat.com/prob/p128270
+    
     public int[] front11(int[] a, int[] b) {
         if (a.length>0 && b.length>0 ) return new int[] {a[0],b[0]};
         else if(a.length>0 ) return new int[]{a[0]};
@@ -9,6 +11,8 @@ public class Array1 {
         return new int[0];
     }
 
+    // https://codingbat.com/prob/p143461
+    
     public int[] make2(int[] a, int[] b) {
        int[] n =  Arrays.copyOf(a,2);
        if(n[0]==0) {n[0]=b[0]; n[1] = b[1];}
@@ -16,22 +20,38 @@ public class Array1 {
        return n;
     }
 
+    
+    // https://codingbat.com/prob/p197308
+    
     public boolean unlucky1(int[] nums) {
         if(nums.length>=2 && ((nums[0]==1 && nums[1]==3)|| (nums[1]==1 && nums[2]==3) ||
                 (nums[nums.length-2]==1&& nums[nums.length-1]==3))) return true;
         return false;
     }
+    
+    // https://codingbat.com/prob/p142455
 
     public int[] frontPiece(int[] nums) {
         if(nums.length>=2) return new int[] {nums[0],nums[1]};
         else if (nums.length==1) return new int[] {nums[0]};
         return new int[0];
     }
+    
+    // https://codingbat.com/prob/p185176
 
     public int maxTriple(int[] nums) {
         Arrays.stream(new int[]{nums[0], nums[nums.length / 2], nums[nums.length - 1]}).max().getAsInt();
     }
+    
+    // https://codingbat.com/prob/p155713
+    
+    public int[] midThree(int[] nums) {
+        return new int[]{nums[nums.length/2 - 1],nums[nums.length/2],  nums[nums.length/2+1]};
+    }
 
+
+    // https://codingbat.com/prob/p118044
+    
     public int[] swapEnds(int[] nums) {
         int t = nums[0];
         nums[0]= nums[nums.length-1];
@@ -39,59 +59,71 @@ public class Array1 {
         return nums;
     }
 
-
+    
+    // https://codingbat.com/prob/p180840
+    
     public int[] plusTwo(int[] a, int[] b) {
-
-         int[] n=Arrays.copyOf(a,4);
+        int[] n=Arrays.copyOf(a,4);
         for (int i = a.length; i < n.length ; i++) {
-            n[i] = b[i-a.length];
+            n[i] = b[i-a.length];          
         }
         return n;
     }
-
-
+    
+    // https://codingbat.com/prob/p199519
+    
     public int[] makeMiddle(int[] nums) {
-
         return new int[]{nums[nums.length/2 - 1], nums[nums.length/2]};
     }
-
+    
+    // https://codingbat.com/prob/p109537
+    
     public int[] biggerTwo(int[] a, int[] b) {
         if (Arrays.stream(a).sum() >= Arrays.stream(b).sum()) return a;
-        return b;
+        return b; 
     }
 
+    // https://codingbat.com/prob/p109660
+    
     public int start1(int[] a, int[] b) {
         int sum = 0;
         if (a.length >0  && a[0]==1 ) sum++;
         if (b.length>0 && b[0]==1)sum++;
         return sum;
     }
-
-
+    
+    // https://codingbat.com/prob/p120347
+    
     public int[] fix23(int[] nums) {
-        if (nums[0]==2 && nums[1]==3) nums[1]=0;
-        if (nums[1]==2 && nums[2]==3) nums[2]=0;
-        return nums;
+      if (nums[0]==2 && nums[1]==3) nums[1]=0;
+      if (nums[1]==2 && nums[2]==3) nums[2]=0;
+      return nums;
     }
-
-    public static boolean double23(int[] nums) {
+    
+    // https://codingbat.com/prob/p145365
+    
+    public boolean double23(int[] nums) {
         return (Arrays.equals(nums,new int[]{2, 2})  || Arrays.equals(nums,new int[]{3, 3}));
     }
 
-
-    public int[] middleWay(int[] a, int[] b) {
-        return new int[] {a[1],b[1]};
-    }
-
-
-
+    // https://codingbat.com/prob/p137188
 
     public int[] makeLast(int[] nums) {
         int i[]= new int[nums.length*2];
         i[i.length-1]=nums[nums.length-1];
         return i;
     }
+    
+    // https://codingbat.com/prob/p175689
+    
+    public boolean no23(int[] nums) {
+        for (int n: nums) {
+            if (n==2 || n==3) return false;
+        }
+        return true;
+    }
 
+    // https://codingbat.com/prob/p171022
 
     public boolean has23(int[] nums) {
         for (int n: nums) {
@@ -99,7 +131,21 @@ public class Array1 {
         }
         return false;
     }
+    
+    // https://codingbat.com/prob/p101230
+    
+    public int[] makeEnds(int[] nums) {
+        return new int[] {nums[0],nums[nums.length-1]};
+    }
+    
+    // https://codingbat.com/prob/p146449
+    
+    public int[] middleWay(int[] a, int[] b) {
+        return new int[] {a[1],b[1]};
+    }
 
+    // https://codingbat.com/prob/p190968
+    
     public int sum2(int[] nums) {
         int[] n = Arrays.stream(nums).limit(2).toArray();
         if(n.length==0) return 0;
@@ -107,7 +153,8 @@ public class Array1 {
     }
 
 
-
+    // https://codingbat.com/prob/p146256
+    
     public int[] maxEnd3(int[] nums) {
         int max = (nums[0]>nums[2])? nums[0] : nums[2];
         for (int i =0; i< nums.length; i++) {
@@ -117,7 +164,8 @@ public class Array1 {
     }
 
 
-
+    // https://codingbat.com/prob/p112409
+    
     public int[] reverse3(int[] nums){
         for (int i = 0; i<nums.length/2;i++){
             int temp = nums[i];
@@ -127,9 +175,9 @@ public class Array1 {
         return nums;
 
 
+    // https://codingbat.com/prob/p185139
 
-
-    public static int[] rotateLeft3(int[] nums) {
+    public int[] rotateLeft3(int[] nums) {
         int rotated[] = new int[nums.length];
         rotated[nums.length-1]=nums[0]
         for(int i=1; i< nums.length;i++){
@@ -138,24 +186,34 @@ public class Array1 {
 
     }
 
+    // https://codingbat.com/prob/p175763        
+        
     public int sum3(int[] nums) {
         return Arrays.stream(nums).sum();
     }
-    public static boolean commonEnd(int[] a, int[] b) {
+        
+    // https://codingbat.com/prob/p191991      
+        
+    public boolean commonEnd(int[] a, int[] b) {
         if(a[0] == b[0] || a[a.length-1] == b.[b.length-1]) return  true;
         return false;
     }
 
-
+    // https://codingbat.com/prob/p167011    
+    
     public int[] makePi() {
         return new int[]{3,1,4};
     }
 
+    // https://codingbat.com/prob/p118976        
+        
     public boolean sameFirstLast(int[] nums) {
         return (nums.length>=1 && nums[0]==nums[nums.length-1]);
     }
 
-    public static boolean firstLast6(int[] nums) {
+    // https://codingbat.com/prob/p185685        
+        
+    public boolean firstLast6(int[] nums) {
         return (nums[0]==6 || nums[nums.length-1]==6);
     }
 }
